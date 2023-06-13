6c9325c2f3b92a3015c03c84cb18268fd36ef9f8
Gabriela Kadlecová <gabi.suchoparova@gmail.com>
2023-06-13 10:30:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='searchspace_train',
     version='0.1',
     license='Apache License 2.0',
-    author='Gabriela Suchopárová',
+    author='Gabriela Kadlecova',
     description='Training networks from common NAS search spaces.',
     packages=setuptools.find_packages(),
     include_package_data=True
